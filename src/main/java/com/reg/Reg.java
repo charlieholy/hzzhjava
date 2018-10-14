7baf183f151ab9ecdedad1b0f837d32378be26b5
陈沛文 <charlieholy@github.com>
2018-10-14 13:15:52 +0800

    add reg

---
+++ src/main/java/com/reg/Reg.java
@@ -0,0 +1,86 @@
+package com.reg;
+
+public class Reg {
+
+    static class RegNormal{
+       public int pc = 0;
+       public int cpsr = 0;
+
+        @Override
+        public String toString() {
+            return " pc: " + pc + " cpsr " + cpsr;
+        }
+    }
+    static class RegFiq{
+        public int lr = 0;
+        public int spsr = 0;
+
+        @Override
+        public String toString() {
+            return " lr " + lr + " spsr " + spsr;
+        }
+    }
+
+    public static RegNormal regNormal;
+    public static RegFiq regFiq;
+
+    static synchronized void irq(){
+        if(Reg.regNormal.cpsr == 0) {
+            Reg.regFiq.spsr = Reg.regNormal.cpsr;
+            Reg.regFiq.lr = Reg.regNormal.pc;
+
+            System.out.println("irq: " + regFiq.toString());
+
+            Reg.regNormal.cpsr = 1;
+            Reg.regNormal.pc = 100;
+        }
+    }
+    static synchronized void normal(){
+        if(Reg.regNormal.cpsr == 1 && Reg.regNormal.pc == 100){
+            Reg.regNormal.cpsr = Reg.regFiq.spsr;
+            Reg.regNormal.pc = Reg.regFiq.lr;
+            System.out.println("normal: " + regNormal.toString());
+        }
+
+
+        //System.out.println("regNo: " + regNormal.toString());
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("nani");
+
+        regNormal = new RegNormal();
+        regFiq = new RegFiq();
+        new Thread(new Runnable() {
+            public void run() {
+                while (true){
+                if(Reg.regNormal.cpsr == 0){
+                System.out.println("pc " + Reg.regNormal.pc++);}
+
+                try{Thread.sleep(1000);}catch (Exception e){}
+            }}
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (true){
+                    try{Thread.sleep(1000);}catch (Exception e){}
+
+                        normal();
+
+
+
+                }
+            }
+        }).start();
+
+        while (true){
+            //set irq
+            irq();
+            try{Thread.sleep(1000);}catch (Exception e){}
+        }
+    }
+
+
+}
